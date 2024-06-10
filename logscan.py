facf2d58fc9f49b9a2521c0d65b93d6c9070be66
Juan Marulanda <jmaruland@bnl.gov>
2024-06-10 18:31:08 -0400

    whitespace

--- logscan.py
+++ logscan.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from prefect import flow, task, get_run_logger
 from tiled.client import from_profile
 
+
 tiled_client = from_profile("nsls2")["srx"]
 tiled_client_raw = tiled_client["raw"]
 
@@ -28,18 +29,18 @@ def logscan_detailed(scanid):
         userdatadir = f"/nsls2/data/srx/proposals/commissioning/{h.start['data_session']}/"
     else:
         userdatadir = f"/nsls2/data/srx/proposals/{h.start['cycle']}/{h.start['data_session']}/"
-        
+
     if not Path(userdatadir).exist():
         logger.info(
             "Incorrect path. Check cycle and proposal id in document. Not running the logger on this document."
         )
         return
-    
+
     logfile_path = userdatadir + f"logfile{h.start['data_session']}.txt"
     is_scanid = False
     if Path(logfile_path).exists():
         is_scanid = find_scanid(logfile_path, h.start['scan_id'])
-    
+
     if not is_scanid:
         with open(logfile_path, "a") as userlogf:
             userlogf.write(str(h.start['scan_id']) + '\t' + str(h.start['uid']) + '\t' + str(h.start['scan']['type']) + '\t' + str(h.start['scan']['scan_input']) + '\n')
