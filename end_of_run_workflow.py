f217d50e744082e7f8ed523c488221124beddb34
Hiran Wijesinghe <wwijesing1@bnl.gov>
2024-06-11 12:42:20 -0400

    pause-data-validation

--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -12,6 +12,6 @@ def log_completion():
 @flow
 def end_of_run_workflow(stop_doc):
     uid = stop_doc["run_start"]
-    data_validation(uid, return_state=True)
+    # data_validation(uid, return_state=True)
     exporter(uid)
     log_completion()
