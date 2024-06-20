007086aa850e2d7c91ab3f60ff6d7c8ef77d0d05
Juan Marulanda <jmaruland@bnl.gov>
2024-06-20 15:34:20 -0400

    Add logscan to end-of-run-workflow

--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -2,6 +2,7 @@ from prefect import task, flow, get_run_logger
 from data_validation import data_validation
 from xanes_exporter import xanes_exporter
 from xrf_hdf5_exporter import xrf_hdf5_exporter
+from logscan import logscan
 
 @task
 def log_completion():
@@ -15,4 +16,5 @@ def end_of_run_workflow(stop_doc):
     # data_validation(uid, return_state=True)
     xanes_exporter(uid)
     xrf_hdf5_exporter(uid)
+    logscan(uid)
     log_completion()
